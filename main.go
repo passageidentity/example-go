17016a1711f08c1df7050930d3615ab2f33a041c
Chris Loper <chris.loper@passage.id>
2021-07-19 09:48:28 -0500

    Add API_KEY SUPPORT:   - getUser

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"text/template"
 
 	"github.com/passageidentity/passage-go"
 )
@@ -20,18 +21,35 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "templates/index.html")
 }
 
+func outputHTML(w http.ResponseWriter, filename string, data interface{}) {
+	t, err := template.ParseFiles(filename)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	if err := t.Execute(w, data); err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+}
+
 func dashboardHandler(w http.ResponseWriter, r *http.Request) {
 	// Authenticate this request using the Passage SDK.
-	psg, err := passage.New("UKbRUx")
+	psg, err := passage.New("UKbRUx", os.Getenv("API_KEY"))
 	if err != nil {
 		fmt.Println("Cannot create psg: ", err)
 	}
-	_, errAuth := psg.AuthenticateRequest(r)
-	if errAuth != nil {
+	userHandle, err := psg.AuthenticateRequest(r)
+	if err != nil {
 		fmt.Println("Authentication Failed:", err)
 		http.ServeFile(w, r, "templates/unauthorized.html")
 		return
 	}
-
-	http.ServeFile(w, r, "templates/dashboard.html")
+	user, err := psg.GetUser(userHandle)
+	if err != nil {
+		fmt.Println("Could not get user: ", err)
+		return
+	}
+	inputArgs := map[string]interface{}{"email": user.Email}
+	outputHTML(w, "templates/dashboard.html", inputArgs)
 }
