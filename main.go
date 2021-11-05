74b64c3e65859fc28b0a4f8765d5cfec3f913a99
apobletts <anna.pobletts@gmail.com>
2021-11-04 21:04:59 -0500

    version of sample app that can be deployed to try-uat

--- main.go
+++ main.go
@@ -1,20 +1,50 @@
 package main
 
 import (
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/base64"
+	"encoding/pem"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"text/template"
 
-	"github.com/passageidentity/passage-go"
+	"github.com/golang-jwt/jwt"
 )
+
+var publicKey *rsa.PublicKey
 
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		log.Fatal("PORT environment variable required")
 	}
+
+	// Get and parse public key
+	// NOTE: THIS IS REQUIRED FOR RUNNING IN NON-PROD, BECAUSE THE SDK IS ONLY SET
+	// UP TO WORK IN PRODUCTION
+	psg_public_key := os.Getenv("PASSAGE_PUBLIC_KEY")
+	if port == "" {
+		log.Fatal("PASSAGE_PUBLIC_KEY environment variable required")
+	}
+
+	// Parse the returned public key string to an rsa.PublicKey:
+	publicKeyBytes, err := base64.RawURLEncoding.DecodeString(psg_public_key)
+	if err != nil {
+		log.Fatal("could not parse Passage App's public key: expected valid base-64")
+	}
+	pemBlock, _ := pem.Decode(publicKeyBytes)
+	if pemBlock == nil {
+		log.Fatal("could not parse Passage App's public key: missing PEM block")
+	}
+	publicKey, err = x509.ParsePKCS1PublicKey(pemBlock.Bytes)
+	if err != nil {
+		log.Fatal("could not parse Passage App's public key: invalid PKCS #1 public key")
+	}
+	// END WEIRD PUBLIC KEY STUFF
 
 	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/dashboard", dashboardHandler)
@@ -41,22 +71,58 @@ func outputHTML(w http.ResponseWriter, filename string, data interface{}) {
 }
 
 func dashboardHandler(w http.ResponseWriter, r *http.Request) {
-	// Authenticate this request using the Passage SDK.
-	psg, err := passage.New(os.Getenv("PASSAGE_APP_ID"), &passage.Config{APIKey: os.Getenv("PASSAGE_API_KEY")})
-	if err != nil {
-		fmt.Println("Cannot create psg: ", err)
-	}
-	userID, err := psg.AuthenticateRequest(r)
+	// Manually validate JWTs with Public Key
+	userID, err := authenticateRequestWithCookie(r)
 	if err != nil {
 		fmt.Println("Authentication Failed:", err)
 		http.ServeFile(w, r, "templates/unauthorized.html")
 		return
 	}
-	user, err := psg.GetUser(userID)
-	if err != nil {
-		fmt.Println("Could not get user: ", err)
-		return
-	}
-	inputArgs := map[string]interface{}{"email": user.Email}
+
+	// PRINT USER ID INSTEAD OF EMAIL IN UAT BECAUSE WE CAN'T USE THE SDK AND IM LAZY
+	inputArgs := map[string]interface{}{"email": userID}
 	outputHTML(w, "templates/dashboard.html", inputArgs)
+}
+
+// AuthenticateRequestWithCookie fetches a cookie from the request and uses it to authenticate
+// returns the userID (string) on success, error on failure
+func authenticateRequestWithCookie(r *http.Request) (string, error) {
+	authTokenCookie, err := r.Cookie("psg_auth_token")
+	if err != nil {
+		return "", errors.New("missing authentication token: expected \"psg_auth_token\" cookie")
+	}
+
+	userID, valid := validateAuthToken(authTokenCookie.Value)
+	if !valid {
+		return "", errors.New("invalid authentication token")
+	}
+
+	return userID, nil
+}
+
+// ValidateAuthToken determines whether a JWT is valid or not
+// returns userID (string) on success, error on failure
+func validateAuthToken(authToken string) (string, bool) {
+	// Verify that the authentication token is valid:
+	parsedToken, err := jwt.Parse(authToken, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodRSA); !ok {
+			return nil, errors.New("invalid signing algorithm")
+		}
+		return publicKey, nil
+	})
+	if err != nil {
+		return "", false
+	}
+
+	// Extract claims from JWT:
+	claims, ok := parsedToken.Claims.(jwt.MapClaims)
+	if !ok {
+		return "", false
+	}
+	userID, ok := claims["sub"].(string)
+	if !ok {
+		return "", false
+	}
+
+	return userID, true
 }
