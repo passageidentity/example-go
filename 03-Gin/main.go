1deb85d7413a8904e3bc7a6b521523006cbdb3e1
Cole Hecht <1923797+colehecht@users.noreply.github.com>
2022-05-23 20:52:39 -0500

    Update code to render email address

--- 03-Gin/main.go
+++ 03-Gin/main.go
@@ -9,13 +9,23 @@ import (
 
 func authRequired() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		psg, _ := passage.New("<Passage App ID>", nil)
-		_, err := psg.AuthenticateRequest(c.Request)
+		psg, _ := passage.New("<Passage App ID>", &passage.Config{
+			APIKey: "<Passage API Key>",
+		})
+		passageUserID, err := psg.AuthenticateRequest(c.Request)
 		if err != nil {
 			// Authentication failed!
 			c.HTML(http.StatusForbidden, "unauthorized.html", nil)
 			c.Abort()
 		}
+
+		// Get the authenticated user's email and set it in the context
+		passageUser, err := psg.GetUser(passageUserID)
+		if err != nil {
+			// This should not fail, but abort the request if it does
+			c.AbortWithStatus(http.StatusInternalServerError)
+		}
+		c.Set("userEmail", passageUser.Email)
 
 		// Authentication was successful, proceed.
 		c.Next()
@@ -33,7 +43,9 @@ func main() {
 	authenticated := r.Group("/", authRequired())
 
 	authenticated.GET("/dashboard", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "dashboard.html", nil)
+		c.HTML(http.StatusOK, "dashboard.tmpl", gin.H{
+			"email": c.MustGet("userEmail").(string),
+		})
 	})
 
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
